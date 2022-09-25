fea9abd8571adf63f459817dc387dec934f28aae
khoerul umam <khoerulumam0723@gmail.com>
2022-09-25 08:37:33 +0700

    route produk

--- Rest-apiTokoweb/routes/api.php
+++ Rest-apiTokoweb/routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
-use App\Http\Controllers\API\ProyekController;
+use App\Http\Controllers\API\ProdukkController;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
@@ -12,4 +12,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::post('register',[AuthController::class,'register']);
 Route::post('login',[AuthController::class,'login']);
 
-Route::apiResource('proyeks',ProyekController::class)->middleware('auth::api');
+Route::apiResource('produks',ProdukController::class)->middleware('auth::api');
